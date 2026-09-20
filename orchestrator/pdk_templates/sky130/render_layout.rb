357107a3aca6042b0210c4e8a35872c95633faa7
balbekov <tim.balbekov@gmail.com>
2026-09-20 16:42:42 +0000

    fix(render): read KLayout runtime globals

--- orchestrator/pdk_templates/sky130/render_layout.rb
+++ orchestrator/pdk_templates/sky130/render_layout.rb
@@ -5,15 +5,15 @@
 # KLayout batch renderer. Runtime variables are supplied with:
 #   -rd input=<DEF-or-GDS> -rd output=<PNG> -rd width=<px> -rd height=<px>
 
-raise "missing input layout" unless defined?(input) && File.file?(input)
-raise "missing output path" unless defined?(output) && !output.empty?
+raise "missing input layout" unless defined?($input) && File.file?($input)
+raise "missing output path" unless defined?($output) && !$output.empty?
 
 app = RBA::Application.instance
 window = app.main_window
-window.load_layout(input, 0)
+window.load_layout($input, 0)
 view = window.current_view
-raise "KLayout did not create a layout view for #{input}" if view.nil?
+raise "KLayout did not create a layout view for #{$input}" if view.nil?
 
 view.max_hier
 view.zoom_fit
-view.save_image(output, width.to_i, height.to_i)
+view.save_image($output, $width.to_i, $height.to_i)
